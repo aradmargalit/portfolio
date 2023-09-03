d8292428607bca447a36c5de35bf4060ac93d342
Arad Margalit <aradmar@gmail.com>
2023-09-03 07:05:02 -0700

    .gitignore for real

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-import profile from '../images/profile.jpeg';
+import profile from './profile.jpeg';
 
 export default function Landing() {
   return (
