9724623ec849db6f9ecfd1ffb3387c9031142542
Arad Margalit <aradmar@gmail.com>
2026-05-25 19:23:05 -0700

    more games items

--- src/app/games/wes/items.ts
+++ src/app/games/wes/items.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 export type QuizItem = {
   name: string;
   url: string;
@@ -11,12 +12,33 @@ export const ITEMS: QuizItem[] = [
   { name: 'Big Bird', url: 'https://sesameworkshop.org/wp-content/uploads/2023/03/bb-p051-sst.png' },
   { name: 'Bert & Ernie', url: 'https://sesameworkshop.org/wp-content/uploads/2023/03/bt-er-p001-sst.png' },
   { name: 'Abby Cadabby', url: 'https://sesameworkshop.org/wp-content/uploads/2023/03/ac-p099-sst.png' },
-  { name: 'Elmo and Abby', url: 'https://sesameworkshop.org/wp-content/uploads/2025/07/EL-AB-p010-SST-300-DPI-JPG-scaled.jpg' },
+  {
+    name: 'Elmo and Abby',
+    url: 'https://sesameworkshop.org/wp-content/uploads/2025/07/EL-AB-p010-SST-300-DPI-JPG-scaled.jpg',
+  },
   { name: 'Bob the Builder', url: 'https://static.wikia.nocookie.net/btb/images/a/a9/Example.jpg' },
   { name: 'Spud', url: 'https://i.redd.it/llq0d1ctb4l41.jpg' },
   { name: 'Olaf', url: 'https://pngimg.com/uploads/frozen/frozen_PNG97.png' },
-  { name: 'Buffalo', url: 'https://upload.wikimedia.org/wikipedia/commons/5/51/Indian_Water_Buffalo_Bubalus_arnee_by_Dr_Raju_Kasambe_IMG_0347_%2811%29_%28cropped%29.jpg' },
+  {
+    name: 'Buffalo',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/5/51/Indian_Water_Buffalo_Bubalus_arnee_by_Dr_Raju_Kasambe_IMG_0347_%2811%29_%28cropped%29.jpg',
+  },
   { name: 'Remy', url: 'https://www.therarebreedmeatcompany.co.uk/wp-content/uploads/2022/08/Untitled-design.jpg' },
   { name: 'Lightning McQueen', url: 'https://upload.wikimedia.org/wikipedia/en/8/82/Lightning_McQueen.png' },
   { name: 'Mater', url: 'https://standingstills.com/cdn/shop/products/CAC357-1-1.jpg?v=1629418790' },
+  {
+    name: 'Trash Truck',
+    url: 'https://occ-0-8407-2218.1.nflxso.net/dnm/api/v6/9pS1daC2n6UGc3dUogvWIPMR_OU/AAAABT5Tmoxx35cACoPbKoX-r0XGXF0Cjmm5S241X3avSvIxcK4sQDGupI-Kx5kV4iIQQr6SxUF2oGuv9ZBTiTkvC1U3a7Ohjem1QCkkzzmq5EUcA4Nd5tPGFE2mzg.jpg?r=e41',
+  },
+  { name: 'Eddie', url: 'https://i.ytimg.com/vi/LI50s4sJkl0/maxresdefault.jpg' },
+  { name: 'Moana', url: 'https://upload.wikimedia.org/wikipedia/en/5/56/Moana_%28character%29.png' },
+  { name: 'Maui', url: 'https://images.thedirect.com/media/photos/maui2.png' },
+  {
+    name: 'Huckle and Sally',
+    url: 'https://www.growingtreetoys.com/cdn/shop/files/a3667293de7af5d9f71c3911a5f797b7601e06733f5eff597c8b3e3f4983ff0c_580x.webp?v=1690421027',
+  },
+  { name: 'Little People', url: 'https://s.wsj.net/public/resources/images/PJ-BN405_DOLLS_F_20130327175214.jpg'},
+  { name: 'Simba', url: 'https://books.disney.com/content/uploads/2024/02/9781368099752.jpg'},
+  { name: 'Scar', url: 'https://upload.wikimedia.org/wikipedia/en/4/4d/Scar_lion_king.png'},
+  { name: 'Timon and Pumbaa', url: 'https://miro.medium.com/v2/resize:fit:2400/0*GapY0K3kzgOTUedZ.jpg'}
 ];
